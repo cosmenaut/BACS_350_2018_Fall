d50676c6ce4d20c26e579a214bbc827d9f57648f
cosme <32144957+cosmenaut@users.noreply.github.com>
2018-10-19 14:14:39 -0600

    quiz 2

---
+++ project/quiz2/index.php
@@ -0,0 +1,10 @@
+<?php
+
+    // Start the page
+    require_once 'views.php';
+ 
+    $site_title = 'Cosme';
+    $page_title = 'QUIZ 2';
+    begin_page($site_title, $page_title);
+    end_page();
+?>
