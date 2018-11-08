4ecfe5b0fe63b22b5dcb564a7acdf8051b9204b0
Mark Seaman <Mark.Seaman@imac.net>
2018-11-08 12:58:26 -0700

    Show toolbar on private

--- solution/30/private.php
+++ solution/30/private.php
@@ -8,36 +8,39 @@
     // Log the page load
     $log->log_page();
 
+    // Display the page content
+    $content = render_button('Templates', '../../templates');
+    $content .= render_button('Solutions', '..');
+    $content .= render_button('Show Log', 'pagelog.php');
+    
 
+    // Check on login
     $login = handle_auth_actions();
     if (empty($login)) {
         require_login('private.php');
         
-        // Display the page content
-        $content = render_button('Templates', '../../templates');
-        $content .= render_button('Solutions', '..');
-        $content .= render_button('Show Log', 'pagelog.php');
-        $content .= render_button('Login', 'private.php?action=login');
         $content .= render_button('Logout', 'private.php?action=logout');
-        $content .= render_button('Sign Up', 'private.php?action=signup');
+        
+        $content .= '
+        <h2>Private Page</h2>
+        <p>
+            This solution demonstrates the use of authentication code.
+            Visiting this page requires a login.
+
+            <a href="index.php">Public Page</a>
+        </p>
+        ';
     }
     else {
-        $content = $login;
+        $content .= render_button('Login', 'private.php?action=login');
+        $content .= render_button('Sign Up', 'private.php?action=signup');
+        $content .= $login;
     }
 
 
     // Display login info
     $content .= show_login();
 
-    $content .= '
-    <h2>Private Page</h2>
-    <p>
-        This solution demonstrates the use of authentication code.
-        Visiting this page requires a login.
-
-        <a href="index.php">Public Page</a>
-    </p>
-    ';
     
 
     // Create main part of page content
