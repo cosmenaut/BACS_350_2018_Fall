ae4c4f5862af8cc660c60e99d4b563b7c328d452
Mark Seaman <Mark.Seaman@imac.net>
2018-11-08 11:39:16 -0700

    gs

--- solution/30/private.php
+++ solution/30/private.php
@@ -13,18 +13,35 @@
     $content = render_button('Templates', '../../templates');
     $content .= render_button('Solutions', '..');
     $content .= render_button('Show Log', 'pagelog.php');
-    
+    $content .= render_button('Login', 'index.php?action=login');
+    $content .= render_button('Logout', 'index.php?action=logout');
+    $content .= render_button('Sign Up', 'index.php?action=signup');
+
+    // Try this login
+
+    //    $email = "me@here.com";
+    //    $password = 'Rock on dude!';
+    //
+    //    // User Setup:
+    //    // require_once 'db.php';
+    //    // register_user($db, $email, $password, 'New', 'User');
+    //        
+    //    $content .= $auth->show_valid ($email, $password);
+
+    $content .= $auth->handle_actions();
+
+
+    $content .= show_login();
 
     content .= '
-    <h2>Public Page</h2>
+    <h2>Private Page</h2>
     <p>
         This solution demonstrates the use of authentication code.
-        Visiting this page does not require a login.
+        Visiting this page requires a login.
 
-        <a href="private.php">Private Page</a>
+        <a href="index.php">Public Page</a>
     </p>
     ';
-    
 
     // Create main part of page content
     $settings = array(
