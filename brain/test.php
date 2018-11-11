4a34b9a7be3522e572527df234f54b9b685a4340
Mark Seaman <Mark.Seaman@imac.net>
2018-11-11 09:47:11 -0700

    Test card views

--- brain/test.php
+++ brain/test.php
@@ -6,7 +6,10 @@
     
 
     // Page content
-    $content = render_card('Page Rendering', 'Experiment with page rendering, before proceeding to the next steps.');
+    $content = 
+        render_card('Page Rendering', 'Experiment with page rendering, before proceeding to the next steps.') .
+        render_card('Preformatted Text', '<pre> function render() { do_this(); }</pre>') .
+        render_card('Markdown', render_markdown("* Bullet lists\n* Styles\n* Headings"));
 
 
     // Create main part of page content
