14b637708e2a1065f67ad95a784f4e3bcfc953ea
Mark Seaman <Mark.Seaman@imac.net>
2018-11-11 09:15:40 -0700

    New custom styles

--- brain/test.php
+++ brain/test.php
@@ -6,14 +6,13 @@
     
 
     // Page content
-    $content = "<h1>Test This Page";
+    $content = render_card('Page Rendering', 'Experiment with page rendering, before proceeding to the next steps.');
 
 
     // Create main part of page content
     $settings = array(
         "site_title" => "Exterior Brain",
         "page_title" => "Test of Dev Features", 
-        "style"      => 'https://getbootstrap.com/docs/4.0/dist/css/bootstrap.min.css',
         "content"    => $content);
 
     echo render_page($settings);
